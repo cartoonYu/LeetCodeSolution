494be90765b18504ad5a4366ab59c905c3de6d48
cartoonYu <13414156340@163.com>
2019-05-13 22:18:34 +0800

    Add Solution 349

---
+++ src/main/java/Solution349.java
@@ -0,0 +1,43 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Solution349 {
+
+    /**
+     * 1.About Complexity:Time Complexity is O(n),Space Complexity is O(n)
+     * 2.how I solve
+     *      2.1 First compare two array,use the shorter array as a compared set
+     *      2.2 Second use the longer array as a compare array
+     *      2.3 Third traverse the compare array,use every element to contrast with the compared set,if it didn't,put the element to a new Set
+     *      2.4 Fourth use iteration traversal to put every element to array
+     * 3.About submit record
+     *      3.1 use 8ms and 38MB memory in LeetCode China
+     *      3.2 use 2ms and 35.4MB memory in LeetCode
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set=new HashSet<>();
+        int[] tempNum=nums1.length>nums2.length?nums1:nums2;
+        int[] tempNum2=nums1.length<nums2.length?nums1:nums2;
+        for(int temp:tempNum){
+            set.add(temp);
+        }
+        Set<Integer> tempSet=new HashSet<>();
+        for(int temp:tempNum2){
+            if(set.contains(temp)){
+                tempSet.add(temp);
+            }
+        }
+        int[] result=new int[tempSet.size()];
+        int flag=0;
+        Iterator<Integer> it=tempSet.iterator();
+        while(it.hasNext()){
+            result[flag++]=it.next();
+        }
+        return result;
+    }
+}
